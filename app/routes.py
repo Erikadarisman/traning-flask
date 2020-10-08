f31ba9e04c201d7fd1c4487cabe18fe403dce2b0
Eri Kadarisman <erikadarisman@gmail.com>
2020-10-08 22:58:45 +0700

    trying route

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,34 @@
 from app import app
+from markupsafe import escape
+
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return "Hello, World!"
+    return 'Index Page'
+
+
+@app.route('/hello')
+def hello():
+    return "Hello, world!"
+
+
+# @app.route('/user/<username>')
+# def show_user(username):
+#     # show the user profile for that user
+#     return 'User %s' % escape(username)
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % escape(username)
+
+
+@app.route('/post/<int:post_id>')
+def post_id(post_id):
+    return 'Post id %d' % post_id
+
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % escape(subpath)
